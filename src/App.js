1c8b0a19f490ebff3de9fe987c21bffe95c91d93
Bpmauge <briahanamauge@pursuit.org>
2019-11-24 16:17:06 -0500

    added two functions to handle subtracting points and winning with 100 points

--- src/App.js
+++ src/App.js
@@ -6,32 +6,57 @@ class App extends React.Component {
     super();
     this.state = {
       counter: 0,
-      currCount: counter,
-      nextCount: counter +1
+      currCount: 1
     }
   }
 
   addClick = (event) => {
-    let { counter } = this.state;
+    let { counter, currCount } = this.state;
     this.setState({
-      counter: counter + 1
+      counter: counter + currCount
     })
+  }
+  checkWin = (event) => {
+    let { counter } = this.state;
+    if(counter >= 100){
+      window.alert(`you win!`)
+    } else{
+      this.addClick();
+    }
   }
 
   subClick = (event) => {
-    let { counter } = this.state;
+    let { counter, currCount } = this.state;
     this.setState({
-      counter: counter - 10
-    })
+      counter: counter - 10,
+      currCount: currCount + 1
+    }); 
+  }
+  enoughPoints = (event) => {
+    let { counter } = this.state;
+    if (counter > 0 && counter >= 10){
+      this.subClick(event)
+    } else{
+      window.alert('not enough points')
+    }
+  }
+
+  resetClick = (event) => {
+    this.setState({
+      counter: 0,
+      currCount: 1
+    });
   }
 
   render() {
-    let { counter } = this.state
+    let { counter, currCount } = this.state
     return (
       <div className="App">
         <p>Current Score: {counter}</p>
-        <button onClick={this.addClick}>+</button>
-        <button onClick={this.subClick}>Pay 10 points to upgrade from {counter} to {counter + counter}</button>
+        <button onClick={this.checkWin}>+</button>
+        <button onClick={this.enoughPoints}>Pay 10 points to upgrade from {currCount} to {currCount + 1}</button>
+        <button onClick={this.resetClick}> Reset</button>
+
       </div>
 
     );
