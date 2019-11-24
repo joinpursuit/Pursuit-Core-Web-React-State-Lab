1269f2c2fd7e33e6267eb73919889efee5d6691f
KameronsCodeSpace <kameronmontague@pursuit.org>
2019-11-24 15:56:01 -0500

    Almost Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+      clickPower: 1
+    }
+  }
+
+  addPoint = (event) => {
+    let counter = this.state.counter;
+    let clickPower = this.state.clickPower;
+    this.setState({
+      counter: counter + clickPower
+    })
+    if(counter >= 100) {
+      window.alert("You won")
+    }
+    // console.log(counter)
+  }
+
+  upgradeButton = () => {
+    let counter = this.state.counter;
+    let clickPower = this.state.clickPower;
+
+    if(counter >= 10) {
+    // console.log("Counter", counter, "ClickPower", clickPower)
+      
+      this.setState({
+        clickPower: clickPower + 1,
+        counter: counter - 10
+      })
+
+    } else {
+      window.alert("Dont have enough points");
+    }
+    // console.log("Counter", counter, "ClickPower", clickPower)
+  }
+
+  render() {
+    // this.handleClick();
+    let { clickPower } = this.state
+    let { counter } = this.state
+
+    return (
+        <div className="App">
+          <p>Current Score</p>
+          <p>{counter}</p>
+          <button className="clicker" onClick={this.addPoint}>{ clickPower }</button>
+          <button className="upgrader" onClick={this.upgradeButton}>Power Up by 1</button>
+        </div>
+    );
+  }
 }
 
 export default App;
