c6cfa3a267c0c4db802e713cf752dd7013f57c3c
vonbarown <vonielbrown@pursuit.org>
2019-11-24 15:27:08 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  addOne = () => {
+    //  console.log('button clicked');
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+
+  subtract = () => {
+    this.setState({
+      counter: this.state.count - 10
+    });
+  };
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div className="App">
+        <h2>Current Score: {count}</h2>
+        <button onClick={this.addOne}>{count}</button>
+        <button onClick={this.subtract}>Pay 10 points to change from{+count} to {count + 1}</button>
+      </div >
+    );
+  }
 }
 
 export default App;
