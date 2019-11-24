17c3be63b0173f1b9363549fef791739a1a38348
RougeLight <dantaeflowers@pursuit.org>
2019-11-24 14:19:08 -0500

    Initial push/ buttons created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (){
+    super();
+    this.state = {
+      score: 0,
+      value: 1
+    }
+  }
+  handleClick = (event) => {
+    let score = this.state.score;
+    let value = this.state.value;
+
+    this.setState({
+      score: score + value,
+    
+    })
+  }
+  
+  increaseValue = (event) => {
+    let score = this.state.score;
+    let value = this.state.value
+  
+    this.setState({
+      score: score - 10,
+      value: value + 1
+    })
+    return value
+  }
+  checkValue = (event) => {
+    let score = this.state.score;
+
+    if (score >= 10) {
+        this.increaseValue()
+    }
+    
+  }
+
+  render() {
+    let score = this.state.score;
+    let value = this.state.value;
+    return (
+      <div className="App">
+        <p>{score}</p>
+        <button onClick={this.handleClick}>Click Me!</button>
+        <button onClick={this.checkValue}>Spend 10 points to increase button value from +{value} to +{value + 1}</button>
+      </div>
+    );
+  }
 }
 
 export default App;
