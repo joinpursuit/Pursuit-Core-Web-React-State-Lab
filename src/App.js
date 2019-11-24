b303f3ef7c55f02c752f945fcba8781e5890a17d
Bpmauge <briahanamauge@pursuit.org>
2019-11-24 15:30:02 -0500

    function for add and subtract button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    }
+  }
+
+  addClick = (event) => {
+    let { counter } = this.state;
+    this.setState({
+      counter: counter + 1
+    })
+  }
+  
+  subClick = (event) => {
+    let { counter } = this.state;
+    this.setState({
+      counter: counter - 10
+    })
+  }
+
+  render() {
+    let { counter } = this.state
+    return (
+      <div className="App">
+        <p>Current Score: {counter}</p>
+        <button onClick={this.addClick}>+</button>
+        <button onClick={this.subClick}>Pay 10 points to upgrade from {counter} to {counter + counter}</button>
+      </div>
+
+    );
+
+  }
 }
 
 export default App;
