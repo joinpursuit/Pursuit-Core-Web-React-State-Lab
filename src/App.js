b0f48bfd337cb96fdbefbd7fb6659fe2faf650f1
aransagarcia <aransagarcia@pursuit.org>
2019-11-24 16:41:25 -0500

    reset button

--- src/App.js
+++ src/App.js
@@ -7,48 +7,60 @@ class App extends React.Component {
     super();
     this.state = {
       score: 0,
-       incrementNum: 1, 
-       pay:10
+      incrementNum: 1,
+      pay: 10
     }
   }
   handleAdd = (event) => {
     let score = this.state.score;
     let incrementNum = this.state.incrementNum
-    if (score >=100){
-      window.alert('YOU WON!')
-      return
-    }
     this.setState({
       score: score + incrementNum
     })
   }
   handlePay = (event) => {
     let { score, incrementNum, pay } = this.state;
-    if (score < 10){
+    if (score < 10) {
       console.log('error not enough')
       window.alert('Not enough points');
       return
     }
     this.setState({
       score: score - pay,
-      incrementNum: incrementNum +1
+      incrementNum: incrementNum + 1
     })
   }
+
+  resetGame = (event) => {
+    this.setState({
+      score: 0,
+      increaseNum: 1
+    })
+  }
+
+
+
+
   render() {
-    // let score = this.state.score;
     let { score, incrementNum, pay } = this.state;
-
-    return (
-      <div className="App">
-
-        <p> Current Score : {score}</p>
-        <button onClick={this.handleAdd}>+ {incrementNum} </button> <br></br>
-        <button onClick={this.handlePay}>Pay {pay} to change +{incrementNum} to +{incrementNum +1} </button>
-
-      </div>
-    );
+    if (score >= 100) {
+      return (
+        <div className="App">
+         <h1>Current Score: {score}</h1>
+         <h1>You WON</h1>
+         <button onClick ={this.resetGame}> RESET</button>
+         </div>
+      )
+    }else {
+      return (
+        <div className ="App">
+          <p> Current Score : {score}</p>
+          <button onClick={this.handleAdd}>+ {incrementNum} </button> <br></br>
+          <button onClick={this.handlePay}>Pay {pay} to change +{incrementNum} to +{incrementNum + 1} </button>
+        </div>
+      ) 
+    }
   }
 }
 
-
-export default App;
+  export default App;
