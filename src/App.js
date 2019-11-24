61ada1ee4e79b18fcb88678b1ddbd8527e8505c0
jenama <johanneenama@gmail.com>
2019-11-24 17:38:01 -0500

    react states lab

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      score: 0,
+      increaseIncrement: 1,
+     
+    }
+  }
+
+  incrementScore = () => {
+    let { score } = this.state
+    this.setState({
+    score: score + 1
+    })
+    console.log('this increases the score', score)
+  }
+
+  useCoins= () => {
+    let {score, increaseIncrement } = this.state
+      if (score < 10) {
+        window.alert(`You don't have enough coins`)
+        return
+      }
+
+    this.setState({
+      score: score - 10,
+      increaseIncrement: increaseIncrement + 1
+
+    })
+  }
+ 
+  render(){
+    let {
+        score,
+          increaseIncrement
+        } = this.state
+        let increment = increaseIncrement
+        let newIncrement = increment + 1
+        console.log('new increment', newIncrement)
+
+    if (score >= 100) { 
+      return(
+        <div className= 'App'>
+            <h2>You won</h2>
+          <button onClick={this.resetGame}> Restart Game</button>
+        </div>
+      )
+    } else {
+
+    return(
+      <div className="App">
+        <div className={increment}>
+          <p> Current  Score: {score}</p>
+          <button onClick={this.incrementScore}>+{increaseIncrement}</button>
+          <br></br>
+          <button onClick={this.useCoins}>Pay 10 points to change from +{increment} to +{newIncrement}</button>
+        </div>
+      </div>
+         
+    )
+  }
+    }
 }
+
+
+
 
 export default App;
