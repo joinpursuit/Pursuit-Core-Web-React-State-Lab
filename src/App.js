d8955a53bdfebed4d8e347f75a7d0e8e3362824c
PeterFiorentino <peterfiorentino@pursuit.org>
2019-11-24 15:21:27 -0500

    Peter Fiorentino

--- src/App.js
+++ src/App.js
@@ -2,25 +2,63 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+      payFor: 1
+    }
+  }
+
+  handleClick = () => {
+    let counter = this.state.counter;
+    let payFor = this.state.payFor;
+    this.setState({
+        counter: counter + payFor
+      })
+      console.log(counter)
+  }
+
+  pay = () => {
+    let counter = this.state.counter;
+    let price = 10;
+    let payFor = this.state.payFor;
+
+    if(counter >= 10) {
+      this.setState({
+        counter: counter - price,
+        payFor: payFor + 1
+      })
+    } else {
+      window.alert("You don't have enough points to do that!")
+    }
+  }
+
+  restart = () => {
+    this.setState({
+      counter: 0,
+      payFor: 1
+    })
+  }
+
+    render() {
+      let counter = this.state.counter
+      if(counter >= 100) {
+        return(<h1>You won!</h1>)
+
+      } else {
+        return (
+          <div className="App">
+            <p>{this.state.counter}</p>
+            <button onClick={this.handleClick}>+</button>
+            <button onClick={this.pay}>Pay 10 points to change +{this.state.payFor} to +{this.state.payFor + 1}</button>
+            <button onClick={this.restart}>Restart</button>
+          </div>
+        );
+      }
+  }
 }
+
 
 export default App;
