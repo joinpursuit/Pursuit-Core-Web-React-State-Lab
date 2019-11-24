ca71affb500e477cc998e3314f3d22f357c3a90e
Owen Jones <owenthefirst93@gmail.com>
2019-11-24 17:41:49 -0500

    finished lab

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      countIncrement: 1,
+      incrementToll: 10
+    }
+  }
+
+    handleIncreaseCount = () => {
+      let {count, countIncrement} = this.state
+      this.setState({
+        count: count + countIncrement
+      })
+    }
+
+    handleIncreaseCountIncrement = () => {
+      let {count, countIncrement} = this.state
+
+      if(count > 9){
+        this.setState({
+          count: count - 10,
+          countIncrement: countIncrement + 1
+        })
+      } else{
+        window.alert('Not enough points to spend')
+      }
+      
+    }
+
+    handleClearScore = () => {
+      let {count, countIncrement} = this.state
+      this.setState({
+        count: count - count,
+        countIncrement: countIncrement - (countIncrement - 1)
+      })
+    }
+    restartGame = () => {
+      window.location.reload()
+    }
+    
+    render() {
+    let {count, countIncrement, incrementToll} = this.state
+
+    if(count > 99){
+      return (
+        <div className="App">
+          <div className='score'>
+            <h1>Current Score: {count}</h1>
+          </div>
+          <div>
+            <button onClick= {this.restartGame}>Restart Game</button>
+          </div>
+          <div>
+            <h3>You Win!!!</h3>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div className="App">
+          <div className='score'>
+            <h1>Current Score: {count}</h1>
+          <div> 
+            <button onClick= {this.handleIncreaseCount}>+{countIncrement}</button>
+          </div>         
+          <div>
+            <button onClick= {this.handleIncreaseCountIncrement}>Spend {incrementToll} points to increase from +{countIncrement} to +{countIncrement + 1} </button>
+          </div> 
+          <div>
+            <button onClick= {this.handleClearScore}>Clear Score</button>
+          </div>  
+          </div>
+        </div>
+      );
+    }
+      
+    }
 }
 
 export default App;
