5545b777983b106d5adad7321119cf87b62c4d47
jonathan <jonathanfagan@pursuit.org>
2019-11-24 16:02:31 -0500

    core functions done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      counter: 0,
+      points: 1
+    }
+  }
+      
+  handleClickAdd = () =>{
+    let counter = this.state.counter;
+    let points = this.state.points;
+
+
+    //don't do this
+    //this.state.counter = counter + 1
+    
+    //do this instead
+    this.setState({
+      counter: counter + points
+    })
+  }
+  
+  handlePayPoints = () =>{
+    let {counter, points } = this.state;
+    
+    this.setState ({
+      counter: counter - 10,
+      points: points +1
+    })
+  }
+  
+  handleClickReset = () =>{
+    
+    this.setState({
+      counter: 0,
+      points: 1
+    })
+  }
+  
+  render(){
+    let counter = this.state.counter
+    let points = this.state.points
+    return (
+      <div className="App">
+        <p>Current Score: {counter}</p>
+        <button onClick ={this.handleClickAdd}>+{points}</button>
+        <button onClick ={this.handlePayPoints}>Pay 10 points to go from +{points} to +{points + 1}</button>
+        <button onClick = {this.handleClickReset}>reset</button>
+      </div>
+    );
+  }
+        
+
+  
+  
 }
 
 export default App;
