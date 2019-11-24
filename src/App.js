5b27fea038cfedc9cb7b31deec9c7aa3207d51de
Bpmauge <briahanamauge@pursuit.org>
2019-11-24 16:39:19 -0500

    fixed the if statement in the render function

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      counter: 0,
+      counter: 90,
       currCount: 1
     }
   }
@@ -16,27 +16,19 @@ class App extends React.Component {
       counter: counter + currCount
     })
   }
-  checkWin = (event) => {
-    let { counter } = this.state;
-    if(counter >= 100){
-      window.alert(`you win!`)
-    } else{
-      this.addClick();
-    }
-  }
 
   subClick = (event) => {
     let { counter, currCount } = this.state;
     this.setState({
       counter: counter - 10,
       currCount: currCount + 1
-    }); 
+    });
   }
   enoughPoints = (event) => {
     let { counter } = this.state;
-    if (counter > 0 && counter >= 10){
+    if (counter > 0 && counter >= 10) {
       this.subClick(event)
-    } else{
+    } else {
       window.alert('not enough points')
     }
   }
@@ -50,17 +42,27 @@ class App extends React.Component {
 
   render() {
     let { counter, currCount } = this.state
+    if (counter >= 100) {
+      return (
+        <div className="App">
+          <div className='win'>
+          <p>Current Score: {counter}</p>
+            <p>You Win</p>
+            <button onClick={this.resetClick}> Reset</button>
+          </div>
+        </div>
+      )
+    }
     return (
       <div className="App">
-        <p>Current Score: {counter}</p>
-        <button onClick={this.checkWin}>+</button>
-        <button onClick={this.enoughPoints}>Pay 10 points to upgrade from {currCount} to {currCount + 1}</button>
-        <button onClick={this.resetClick}> Reset</button>
-
+        <div className='game'>
+          <p>Current Score: {counter}</p>
+          <button onClick={this.addClick}>+{currCount}</button>
+          <button onClick={this.enoughPoints}>Pay 10 points to upgrade from {currCount} to {currCount + 1}</button>
+        </div>
       </div>
 
     );
-
   }
 }
 
