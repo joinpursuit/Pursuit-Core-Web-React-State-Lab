fa0182bf17f9c5901933dacbd1745069b0af95da
sergiocohens <sergiocohensalama@pursuit.org>
2019-11-24 19:17:25 -0500

    lab

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      counter: 0,
+      increase: 1,
+      cost: 10
+    }
+  }
+
+  playAgain = () => {
+    window.location.reload(false)
+  }
+
+  handleClick = () => {
+    let { counter, increase } = this.state
+
+    if (counter >= 99) {
+      const youWin = (
+      <h1 id='won'>You Won!</h1>
+    )
+      const playAgain = (
+      <button onClick={this.playAgain}>Play again</button>
+    )
+      ReactDOM.render(youWin, document.getElementById('won'))
+      ReactDOM.render(playAgain, document.getElementById('again'))
+    } else {
+      this.setState({
+      counter: counter + increase
+    })
+    }
+  }
+
+  increaseValue = () => {
+    let { counter, increase, cost} = this.state
+
+    if (counter >= cost) {
+      this.setState({
+        counter: counter - cost,
+        increase: increase + 1,
+        cost: cost + 10
+      })
+    } else {
+      window.alert("you can't afford that!")
+    }
+  }
+
+  render() {
+    let { counter, cost, increase } = this.state
+
+    return (
+      <div className="App" id='main'>
+      <h1> Current score: {counter}</h1>
+      <div id='won'></div>
+      <div id='again'></div>
+      <br></br>
+      <button onClick={this.handleClick}>+{increase}</button>
+      <br></br>
+      <br></br>
+      <button onClick={this.increaseValue}>Pay {cost} points to change from +{increase} to +{increase + 1}</button>
+      </div>
+    )
+  }
 }
 
 export default App;
