5ab8069894c7d0514439c3ca49c6e65d84c0003d
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-11-25 02:44:30 -0500

    0.2, App work started

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+Joseph P. Pasaoa
+Clicker App | React State Lab
+*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+/* IMPORTS */
+    // components
+import React from 'react';
+    // css
+import './App.css';
+    // helpers
+let { log } = require('../utils/helpers.js');
+    
+    
+/* BODY */
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
+
+  handleClick = () => {
+    let counter = this.state.counter;
+    this.setState({
+      counter: counter + 1
+    });
+  }
+
+  render() {
+    return (
+      <div id="base-grid">
+        
+      </div>
+    );
+  }
 }
+
 
 export default App;
