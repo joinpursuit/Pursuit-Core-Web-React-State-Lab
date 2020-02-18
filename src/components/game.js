d7cd83784cf7af16209aaf048c338b5de82303fc
david maravillas <davidmaravillas@pursuit.org>
2020-02-18 06:40:33 -0500

    add step upgrade button

--- src/components/game.js
+++ src/components/game.js
@@ -11,12 +11,22 @@ class Game extends React.Component{
             currentScore:this.state.currentScore+this.state.currentStep
         })
     }
+    handleStepUpgrade=()=>{
+        if(this.state.currentScore>=10){
+
+            this.setState({
+                currentScore:this.state.currentScore-10,
+                currentStep:this.state.currentStep+1
+            })
+        }
+    }
 
     render(){return(
         <div>
             <h3>current Score {this.state.currentScore}</h3>
 
-            <input type="button" value={this.state.currentStep} onClick={this.handleStep}/>
+            <input type="button" value={`+${this.state.currentStep}`} onClick={this.handleStep}/>
+            <input type="button" value={`pay 10 pts to change +${this.state.currentStep} to +${this.state.currentStep+1}`} onClick={this.handleStepUpgrade}/>
         </div>
     )}
 
