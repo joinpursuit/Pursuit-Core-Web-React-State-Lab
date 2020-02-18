5d253f07d8a3dc5748d6a0447cb4ba47d7f7eacb
david maravillas <davidmaravillas@pursuit.org>
2020-02-18 13:29:53 -0500

    add restart function

--- src/components/game.js
+++ src/components/game.js
@@ -3,13 +3,18 @@ import React from "react"
 
 class Game extends React.Component{
     state={
-        currentScore:0,
-        currentStep:1
+        currentScore:99,
+        currentStep:1,
+        gameWon: "",
+        showWin:"none"
     }
     handleStep=()=>{
         this.setState({
             currentScore:this.state.currentScore+this.state.currentStep
         })
+        if(this.state.currentScore >= 100){
+            return this.handleWin()
+        }
     }
     handleStepUpgrade=()=>{
         if(this.state.currentScore>=10){
@@ -18,15 +23,41 @@ class Game extends React.Component{
                 currentScore:this.state.currentScore-10,
                 currentStep:this.state.currentStep+1
             })
+        } else{
+            alert(`user can't afford this change`)
         }
+    }
+
+    handleWin=()=>{
+        this.setState({
+                gameWon:'none',
+                showWin:''
+            })
+    }
+    handleRestart=()=>{
+        this.setState({
+                currentScore:99,
+                currentStep:1,
+                gameWon: "",
+                showWin:"none"
+            })
     }
 
     render(){return(
         <div>
-            <h3>current Score {this.state.currentScore}</h3>
+            <h3>CURRENT SCORE: {this.state.currentScore}</h3>
 
-            <input type="button" value={`+${this.state.currentStep}`} onClick={this.handleStep}/>
-            <input type="button" value={`pay 10 pts to change +${this.state.currentStep} to +${this.state.currentStep+1}`} onClick={this.handleStepUpgrade}/>
+            <div style={{display:`${this.state.gameWon}`}} >
+
+                <input type="button" value={`+${this.state.currentStep}`} onClick={this.handleStep} display=""/>
+                <input type="button" value={`pay 10 pts to change +${this.state.currentStep} to +${this.state.currentStep+1}`} onClick={this.handleStepUpgrade}/>
+            </div>
+            <div style={{display:`${this.state.showWin}`}} >
+                <h3> YOU WIN! </h3>
+                <input type="button" value="play agin" onClick={this.handleRestart}/>
+
+            </div>
+
         </div>
     )}
 
