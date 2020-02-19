d626213cc59ca96495b62bee8a05dc432f81acda
DejaLFlynn <dejaflynn@pursuit.org>
2020-02-19 18:47:34 -0500

    linking components and start of Class and methods

---
+++ src/Components/Game.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Game.css'
+class Game extends React.Component{
+state={
+    score:0,
+    points:1
+}
+render(){
+
+}
+handleStart = ()=>{
+    
+}
+}
+
+
+export default Game;
