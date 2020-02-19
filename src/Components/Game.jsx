b5ec56da3219ee43acdb30f350704a409134df4f
DejaLFlynn <dejaflynn@pursuit.org>
2020-02-19 18:58:38 -0500

    working on methods

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -5,12 +5,36 @@ state={
     score:0,
     points:1
 }
-render(){
+handleStart = ()=>{
 
 }
-handleStart = ()=>{
-    
+handlePoints =()=>{
+
 }
+handleBonus = ()=>{
+
+}
+render(){
+    if(this.state.score >2){
+        <>
+        <h2>Score: {this.state.score} !</h2>
+        <h1>You Win</h1>
+        <button onClick={this.handleStart}>Start</button>
+
+        </>
+    }else{
+        return(
+            <>
+    <h2>Score: {this.state.score} !</h2>
+    <button onClick={this.handlePoints}>Add {this.state.points} </button>
+    <button onClick={this.handleBonus}>You cant afford that!</button>
+            </>
+        )
+    }
+
+}
+
+
 }
 
 
