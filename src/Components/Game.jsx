5f350a2285839e52595ae8b8dec70870f5b9b8f4
DejaLFlynn <dejaflynn@pursuit.org>
2020-02-19 19:57:03 -0500

    finished lab

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,41 +1,53 @@
 import React from 'react';
 import './Game.css'
-class Game extends React.Component{
-state={
-    score:0,
-    points:1
-}
-handleStart = ()=>{
-
-}
-handlePoints =()=>{
-
-}
-handleBonus = ()=>{
-
-}
-render(){
-    if(this.state.score >2){
-        <>
-        <h2>Score: {this.state.score} !</h2>
-        <h1>You Win</h1>
-        <button onClick={this.handleStart}>Start</button>
-
-        </>
-    }else{
-        return(
-            <>
-    <h2>Score: {this.state.score} !</h2>
-    <button onClick={this.handlePoints}>Add {this.state.points} </button>
-    <button onClick={this.handleBonus}>You cant afford that!</button>
-            </>
-        )
+export default class Game extends React.Component {
+    state = {
+        score: 0,
+        points: 1
+    }
+    handleStart = () => {
+        this.setState({
+            score: 0,
+            points: 1
+        })
     }
 
+    handlePoints = () => {
+        this.setState({score: this.state.score + this.state.points})
+    }
+
+    handleBonus = () => {
+        if (this.state.score > 10) {
+            this.setState({
+                points: this.state.points + 1,
+                score: this.state.score - 10
+            })
+        } else {
+            window.alert("You don't have enough points")
+        }
+    }
+
+    render() {
+        if (this.state.score > 2) {
+            return (
+                <>
+                    <h1>Your Score: {this.state.score} points</h1>
+                    <h2>You Win!! </h2>
+                    <button onClick={this.handleStart}>Restart</button>
+                </>
+            );
+        } else {
+            return (
+                <>
+                    <h1>Your Score: {this.state.score} points</h1>
+                    <button onClick={this.handlePoints}>Add {this.state.points} point</button>
+
+                    <hr></hr>
+                </>
+            );
+        }
+    }
 }
 
 
-}
 
-
-export default Game;
